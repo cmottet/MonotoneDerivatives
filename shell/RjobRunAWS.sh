5e112953e641808ea5ec49b5f0aac31f4ad1492e
cmottet <cmottet@cmottets-MacBook-Pro.local>
2017-11-21 17:41:53 -0500

    cleaning codes

--- shell/RjobRunAWS.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Take off the headerline from the line count
-prog_dir="/home/ec2-user/MonotoneDerivatives"
-while read line;
-  do
-  echo $line
-  R -q --save --args $line < $prog_dir""/R/runMinMaxSyntheticTransformed_Hill_Horror_Pareto_GLP.R >> Rjob.out
-done < $prog_dir""/data/log_Hill_data_rows_to_run.txt
